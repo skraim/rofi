a89330e70c05e29aed5efec79734564d6288ddbd
Artem <darkskraim@gmail.com>
2025-06-12 12:04:26 +0300

    add fastfetch screen making

--- files/powermenu/type-2/powermenu.sh
+++ files/powermenu/type-2/powermenu.sh
@@ -70,9 +70,9 @@ run_cmd() {
 	selected="$(confirm_exit)"
 	if [[ "$selected" == "$yes" ]]; then
 		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
+            ~/scripts/fastfetch-screen.sh; systemctl poweroff
 		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
+			~/scripts/fastfetch-screen.sh; systemctl reboot
 		elif [[ $1 == '--suspend' ]]; then
 			mpc -q pause
 			amixer set Master mute
